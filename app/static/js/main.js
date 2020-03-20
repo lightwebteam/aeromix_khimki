15649e0221f5d4481e588b44909828d43aff8719
Alexa <kan_off@mail.ru>
2020-03-20 15:44:06 +0300

    ready

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -5,14 +5,12 @@ $( document ).ready(function() {
     let imgArr = document.querySelectorAll('.fancybox');
     let imgAttr=[];
     let modalContent = document.querySelectorAll('.modal-dialog');
-    let cliHeight = document.clientheight;
+    let cliHeight = document.documentElement.clientHeight;
 
     
     //  GALLERY
     imgArr.forEach( item => {
         imgAttr.push(item.querySelector('img').getAttribute('src'));
-        
-        // genImg(item);
     });
 
     for (let i = 0; i < imgArr.length; i++) {
@@ -25,30 +23,24 @@ $( document ).ready(function() {
         modalContent[i].appendChild(divImg);
     }
 
-    console.log(imgAttr)
-
-    // function genImg(index) {
-    //     let divImg = document.createElement('div');
-    //     let img = document.createElement('div');
-
-    //     img.setAttribute('class', 'Img-js');
-    //     img.style.backgroundImage = `url(${imgAttr[index]})`
-    //     divImg.appendChild(img);  
-    //     modalContent.appendChild(divImg);
-    // }
-
     //       PARALLAX EFFECT
     $(window).on( "scroll", function () {
         let scroll = $(window).scrollTop();
         let kef = scroll * 0.4;
 
         $('.sportgirl').css({'transform' : 'translateZ(-500px) translate(0%,' + kef + 'px) scale(2)'})
-        if ( scroll > 870 ) {
+
+        // NAVBAR
+        if ( scroll > cliHeight ) {
             $('.navbar').addClass( "active" );
-        }else if (cliHeight <= 667) {
-            $('.sportgirl').css({'transform' : 'translateZ(-500px) translate(0%,' + kef + 'px) scale(2)'})
         } else {
             $('.navbar').removeClass("active");
         }
     });
+
+    $('.menu_burger').click(function () {
+        let aria = $('.navbar-toggler').attr( "aria-expanded" );
+        aria = !aria;
+        $('.menu_burger').removeClass("show");
+    })
 });
